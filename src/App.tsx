c5daed609e47f6284e8680d3b7b4efa3c50357aa
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-24 21:30:03 +0700

    add logo + connect phantom wallet button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Logo, Button } from "web3uikit";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Logo color="black" theme="icon" />
+      <Button
+        id="test-button"
+        onClick={function noRefCheck() {}}
+        text="Secondary Button"
+        theme="secondary"
+        type="button"
+      />
+    </div>
+  );
 }
 
 export default App;
