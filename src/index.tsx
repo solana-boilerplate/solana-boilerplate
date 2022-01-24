7653f4ddfc2fd2486d4d2771a2d77f91093c5598
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-24 21:09:48 +0700

    wrap app with MoralisProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { MoralisProvider } from "react-moralis";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_APP_ID ?? ""}
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL ?? ""}
+    >
+      <App />
+    </MoralisProvider>
+  </StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
