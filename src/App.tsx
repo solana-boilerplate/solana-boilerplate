6cf5e873bfd4aea9be827e9b10c95d4c22f6c067
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@pop-os.localdomain>
2022-01-24 22:07:28 +0700

    setup basic layout structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
+import { FC } from "react";
+import { useMoralis } from "react-moralis";
 import { Logo, Button } from "web3uikit";
+import { Layout, Menu } from "antd";
+import "antd/dist/antd.css";
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const App: FC = () => {
+  const { authenticate } = useMoralis();
+
+  const onConnectWallet = async () => {
+    try {
+      // @ts-ignore
+      await authenticate({ type: "sol", onSuccess: () => {} });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <Logo color="black" theme="icon" />
-      <Button
-        id="test-button"
-        onClick={function noRefCheck() {}}
-        text="Secondary Button"
-        theme="secondary"
-        type="button"
-      />
-    </div>
+    <Layout>
+      <Header style={{ display: "flex", justifyContent: "space-between" }}>
+        <Logo color="white" theme="icon" />
+        <Menu theme="light" mode="horizontal">
+          <Menu.Item key="mail">Tokens</Menu.Item>
+          <Menu.Item key="app">NFTs</Menu.Item>
+        </Menu>
+        <Button
+          id="test-button"
+          onClick={onConnectWallet}
+          text="Connect Wallet"
+          theme="secondary"
+          type="button"
+        />
+      </Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
